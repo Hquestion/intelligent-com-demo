b62a6330c96316d9f29b5554a92ba3306f0039df
贺小雷 <xiaolei.he@iluvatar.ai>
2019-03-20 19:17:35 +0800

    11

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'production',
     context: path.resolve(__dirname, '../packages'),
     entry: {
-        dataset: './packages/dataset/index.js'
+        dataset: './Dataset/index.js'
     },
     output: {
         filename: "[name].js",
@@ -16,7 +16,12 @@ module.exports = {
         libraryExport: "default"
     },
     externals: {
-        'vue': 'vue',
+        'vue': {
+            root: 'Vue',
+            commonjs: 'vue',
+            commonjs2: 'vue',
+            amd: 'vue'
+        },
         'vue-router': 'vue-router',
         'element-ui': 'element-ui'
     },
@@ -29,6 +34,27 @@ module.exports = {
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
+            },
+            {
+                test: /\.(jsx?|babel|es6)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', "css-loader"]
+            },
+            {
+                test: /\.scss$/,
+                loader: ['style-loader', "sass-loader"]
+            },
+            {
+                test: /\.(svg|otf|ttf|woff2?|eot|gif|png|jpe?g)(\?\S*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: path.posix.join('static', '[name].[hash:7].[ext]')
+                }
             }
         ]
     }
