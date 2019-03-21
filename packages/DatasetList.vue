85ac5cd2e546ac1b2603d896018366af9797137d
贺小雷 <xiaolei.he@iluvatar.ai>
2019-03-21 15:19:04 +0800

    支持package中集成路由

--- packages/DatasetList.vue
+++ packages/DatasetList.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="sky-dataset-list">
+        这是dataset组件的页面
+        <br>
         <ElButton @click="createDataset">进入创建数据集</ElButton>
     </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="Home">
         this is home page
+        <ElButton type="primary" @click="toDataset">跳转数据集页面</ElButton>
     </div>
 </template>
 
@@ -9,6 +10,13 @@ export default {
     name: 'Home',
     data() {
         return {};
+    },
+    methods: {
+        toDataset() {
+            this.$router.push({
+                name: 'dataset-list'
+            });
+        }
     }
 }
 </script>
